466f66c263b74b5d8ba10b0247664ed05207108f
İlker Ünver <108666062+IlkerUnver00@users.noreply.github.com>
2022-07-06 10:39:40 +0300

    Add files via upload

---
+++ Vektorel.MethodOverLoading.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorel.MethodOverLoading.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float d = Toplama(10.3f, 2);
+            
+        }
+
+
+
+        static double Toplama(double a,double b)
+        {
+            return a + b;
+        }
+
+        static float Toplama(float a,float b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
